3bf13b053946f5097ddf9af04625c42cc11247b2
abb21timsjo <tim.sjostrand@abbgymnasiet.se>
2022-09-07 14:01:59 +0200

    habitat

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,32 @@
   </div>
   <hr class="mt-16">
 
-  <div class="flex justify-center m-16">
+  <div class="flex justify-center m-8">
     most viewed
   </div>
-  <div class="w-1/2">
-    <img src="" alt="">
+<center>
+  <div class="flex justify-center w-1/2">
+    <img class="" src="../static/images/BOP05402a-Dove-Zeebra-1-Raiatea-Isl.-Polynesia-Sept19-PH5_0135.jpg" alt="">
   </div>
+  <div class="m-16 text-2xl w-1/2">
+    Det här är historian om den sällsynta pickfågeln som bara flyger 5 gånger per år.
+  </div>
+</center>
 
+<div class="flex flex-wrap m-32 justify-center">
+  <div class="w-2/5">
+    <img src="https://cdn-icons-png.flaticon.com/512/1842/1842238.png" alt="">
+  </div>
+  <div class="w-2/5">
+    <img src="https://cdn-icons-png.flaticon.com/512/3572/3572155.png" alt="">
+  </div>
+  <div class="mt-8 w-2/5">
+    <img src="https://cdn-icons-png.flaticon.com/512/3254/3254422.png" alt="">
+  </div>
+  <div class="mt-8 w-2/5">
+    <img src="https://cdn-icons-png.flaticon.com/512/3251/3251185.png" alt="">
+  </div>
+</div>
   </div>
 </template>
 
