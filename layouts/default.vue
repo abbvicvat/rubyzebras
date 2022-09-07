c1ddb0e36008349048f9084f9ff642341d2cf254
Victor Vatn <victor.vatn@abbgymnasiet.se>
2022-09-07 10:18:35 +0200

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <header>
+        <nav id="navbar" class="bg-gray pr-16 py-1.5 flex justify-end items-center flex-wrap">
+            <nuxt-link class="mx-10 hoverLink" to="/"> Home </nuxt-link>
+            <nuxt-link class="mx-10 hoverLink" to="/articles"> Articles </nuxt-link>
+        </nav>
+
+    </header>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+};
+</script>
